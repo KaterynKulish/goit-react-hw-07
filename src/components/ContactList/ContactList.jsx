ac2ce2b09109af5068cadc8719a38b658a63fad0
Kateryna Kulish <kulishk26@gmail.com>
2025-02-26 11:20:34 +0200

    HW-7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,14 @@
 import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import s from './ContactList.module.css';
+import { selectFilteredContactsMemo } from '../../redux/contactsSlice';
 
 const ContactList = () => {
-  const contacts = useSelector(state => state.contacts.items);
-  const filter = useSelector(state => state.filter.filter);
-
-  const filteredContact = contacts.filter(item =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const contacts = useSelector(selectFilteredContactsMemo);
 
   return (
     <ul className={s.list}>
-      {filteredContact.map(contact => (
+      {contacts.map(contact => (
         <li key={contact.id} className={s.person}>
           <Contact contact={contact} />
         </li>
